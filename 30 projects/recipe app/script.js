e72f1bbc128ed2426870cd5a74fb987e49025196
GauravKumarGope <121334867+GauravKumarGope@users.noreply.github.com>
2024-10-18 16:14:56 +0530

    Add files via upload

---
+++ 30 projects/recipe app/script.js
@@ -0,0 +1,7 @@
+const searchBox= document.querySelector('.searchBox');
+const searchBtn= document.querySelector('.searchBtn');
+const recipeConatiner= document.querySelector('.recipe-container');
+
+searchBtn.addEventListener('click', ()=>{
+    
+});
